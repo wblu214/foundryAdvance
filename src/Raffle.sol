0f44247f2f1b9fb1c158487ecff40cb7923da4e2
wblu214 <wblu.eric@gmail.com>
2025-12-08 18:18:37 +0800

    update vaiable_name

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -25,6 +25,12 @@ contract Raffle is VRFConsumerBaseV2Plus {
     // 自定义错误：当尝试在开奖间隔时间未到时抽取获胜者时触发
     error WaitNextDrawTime();
 
+    error Raffle_CheckUpKeepNotNeed(
+        uint256 balance,
+        uint256 playersLength,
+        uint256 raffleState
+    );
+
     /**
      * @dev 抽奖状态枚举
      * OPEN - 抽奖开放，参与者可以支付入场费
@@ -36,30 +42,30 @@ contract Raffle is VRFConsumerBaseV2Plus {
     }
 
     // 存储所有参与抽奖的玩家地址
-    address payable[] private s_players;
+    address payable[] private players;
     // 入场费，不可变变量，部署后不可更改
-    uint256 private immutable i_enteranceFee;
+    uint256 private immutable ENTRANCE_FEE;
     // 开奖间隔时间（秒），不可变变量，部署后不可更改
-    uint256 private immutable i_interval;
+    uint256 private immutable INTERVAL;
 
     // Chainlink VRF相关参数
     // VRF密钥哈希，用于标识VRF服务
-    bytes32 private immutable i_keyHash;
+    bytes32 private immutable KEY_HASH;
     // VRF订阅ID，用于支付VRF服务费用
-    uint256 private immutable i_subscriptionId;
+    uint256 private immutable SUBSCRIPTION_ID;
     // VRF请求确认数，确保随机性的可靠性
     uint16 private constant REQUEST_CONFIRMATIONS = 3;
     // 回调函数的Gas限制，防止Gas耗尽
-    uint32 private immutable i_callbackGasLimit;
+    uint32 private immutable CALLBACK_GAS_LIMIT;
     // 请求的随机数数量
     uint32 private constant NUM_WORDS = 1;
     // 最近一次抽奖的获胜者地址
-    address payable private s_recentAddress;
+    address payable private recentWinner;
     // 当前抽奖状态
-    RaffleState private s_raffleState;
+    RaffleState private raffleState;
 
     // 上次开奖时间戳
-    uint256 private s_lastTimeStamp;
+    uint256 private lastTimeStamp;
 
     // 事件：当有玩家参与抽奖时触发
     event RaffleEntered(address indexed player);
@@ -83,14 +89,14 @@ contract Raffle is VRFConsumerBaseV2Plus {
         uint256 subscriptionId,
         uint32 callbackGasLimit
     ) VRFConsumerBaseV2Plus(vrfCoordinator) {
-        i_enteranceFee = entranceFee;
-        i_interval = interval;
-        i_keyHash = gsaLine;
-        i_subscriptionId = subscriptionId;
-        i_callbackGasLimit = callbackGasLimit;
+        ENTRANCE_FEE = entranceFee;
+        INTERVAL = interval;
+        KEY_HASH = gsaLine;
+        SUBSCRIPTION_ID = subscriptionId;
+        CALLBACK_GAS_LIMIT = callbackGasLimit;
 
-        s_raffleState = RaffleState.OPEN;
-        s_lastTimeStamp = block.timestamp;
+        raffleState = RaffleState.OPEN;
+        lastTimeStamp = block.timestamp;
     }
 
     //CEI: Checks, Effects, Interactions Patten
@@ -100,7 +106,7 @@ contract Raffle is VRFConsumerBaseV2Plus {
      * @return 入场费金额
      */
     function getEntranceFee() public view returns (uint256) {
-        return i_enteranceFee;
+        return ENTRANCE_FEE;
     }
 
     /**
@@ -108,7 +114,7 @@ contract Raffle is VRFConsumerBaseV2Plus {
      * @return 上次开奖的时间戳
      */
     function getLastTimeStamp() public view returns (uint256) {
-        return s_lastTimeStamp;
+        return lastTimeStamp;
     }
 
     /**
@@ -117,14 +123,14 @@ contract Raffle is VRFConsumerBaseV2Plus {
      * @notice 只有在抽奖状态为OPEN时才能参与
      */
     function payEntranceFee() public payable {
-        if (msg.value < i_enteranceFee) {
+        if (msg.value < ENTRANCE_FEE) {
             revert NotEnoughEth();
         }
-        if (s_raffleState != RaffleState.OPEN) {
+        if (raffleState != RaffleState.OPEN) {
             revert WaitNextDrawTime();
         }
 
-        s_players.push(payable(msg.sender));
+        players.push(payable(msg.sender));
 
         emit RaffleEntered(msg.sender);
     }
@@ -134,49 +140,63 @@ contract Raffle is VRFConsumerBaseV2Plus {
      * @notice 只有在距离上次开奖时间超过间隔时间时才能调用
      * @notice 将抽奖状态设置为CALCULATING，防止新的参与者加入
      */
-    function pickWinner() public {
-        if ((block.timestamp - s_lastTimeStamp) < i_interval) {
-            revert WaitNextDrawTime();
+    function performUpkeep(bytes calldata /* performData */) external {
+        (bool upkeepNeeded, ) = checkUpkeep("");
+        if (!upkeepNeeded) {
+            revert Raffle_CheckUpKeepNotNeed(
+                address(this).balance,
+                players.length,
+                uint256(raffleState)
+            );
         }
 
-        s_raffleState = RaffleState.CALCULATING;
+        raffleState = RaffleState.CALCULATING;
         VRFV2PlusClient.RandomWordsRequest memory request = VRFV2PlusClient
             .RandomWordsRequest({
-                keyHash: i_keyHash,
-                subId: i_subscriptionId,
+                keyHash: KEY_HASH,
+                subId: SUBSCRIPTION_ID,
                 requestConfirmations: REQUEST_CONFIRMATIONS,
-                callbackGasLimit: i_callbackGasLimit,
+                callbackGasLimit: CALLBACK_GAS_LIMIT,
                 numWords: NUM_WORDS,
                 extraArgs: VRFV2PlusClient._argsToBytes(
                     VRFV2PlusClient.ExtraArgsV1({nativePayment: false})
                 )
             });
-        uint256 requestId = s_vrfCoordinator.requestRandomWords(request);
+        s_vrfCoordinator.requestRandomWords(request);
+    }
+
+    function checkUpkeep(
+        bytes memory /* checkData */
+    ) public view returns (bool upkeepNeeded, bytes memory /* performData */) {
+        bool timeHasPassed = block.timestamp - lastTimeStamp >= INTERVAL;
+        bool hasPlayers = players.length > 0;
+        bool isOpen = raffleState == RaffleState.OPEN;
+        upkeepNeeded = (timeHasPassed && hasPlayers && isOpen);
+        return (upkeepNeeded, "");
     }
 
     /**
      * @dev Chainlink VRF回调函数，处理随机数并选出获胜者
-     * @param _requestId VRF请求ID
      * @param randomWords VRF返回的随机数数组
      * @notice 使用随机数模玩家数量来选择获胜者
      * @notice 将奖池中的所有ETH发送给获胜者
      * @notice 重置抽奖状态，清空玩家列表，更新开奖时间
      */
     function fulfillRandomWords(
-        uint256 _requestId,
+        uint256 /*_requestId*/,
         uint256[] calldata randomWords
     ) internal override {
-        uint256 indexOfWinner = randomWords[0] % s_players.length;
-        address payable recentWinner = s_players[indexOfWinner];
-        s_recentAddress = recentWinner;
+        uint256 indexOfWinner = randomWords[0] % players.length;
+        address payable winner = players[indexOfWinner];
+        recentWinner = winner;
         //将抽奖状态重新置为OPEN
-        s_raffleState = RaffleState.OPEN;
-        s_players = new address payable[](0);
-        s_lastTimeStamp = block.timestamp;
-        (bool success, ) = recentWinner.call{value: address(this).balance}("");
+        raffleState = RaffleState.OPEN;
+        players = new address payable[](0);
+        lastTimeStamp = block.timestamp;
+        (bool success, ) = winner.call{value: address(this).balance}("");
         if (!success) {
             revert NotSendWinnerMoney();
         }
-        emit RaffleWinnerPicked(recentWinner);
+        emit RaffleWinnerPicked(winner);
     }
 }
